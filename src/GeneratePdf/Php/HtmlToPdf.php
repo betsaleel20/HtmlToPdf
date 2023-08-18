e07ad79e56f16f665833653c16897100f10d6218
betsaleel20 <judebetsa111@gmail.com>
2023-08-18 09:33:46 +0100

    [First commit] some text

---
+++ src/GeneratePdf/Php/HtmlToPdf.php
@@ -0,0 +1,75 @@
+<?php
+namespace App\GeneratePdf\Php;
+
+use mikehaertl\wkhtmlto\Pdf;
+
+class HtmlToPdf extends PDF
+{
+
+    public function buildPdf(): bool
+    {
+        $name = "htmlToPdf_" . time() . ".pdf";
+
+        ob_start();
+        require realpath('/opt/lampp/htdocs/crinaserver/WkHtmlToPdf/src/GeneratePdf/sections/header.html');
+        $header = ob_get_clean();
+
+        ob_start();
+        require realpath('/opt/lampp/htdocs/crinaserver/WkHtmlToPdf/src/GeneratePdf/sections/content.html');
+
+        $content = ob_get_clean();
+
+        ob_start();
+        require realpath('/opt/lampp/htdocs/crinaserver/WkHtmlToPdf/src/GeneratePdf/sections/footer.html');
+
+        $footer = ob_get_clean();
+
+        $pdfOptions1 = [
+            'enable-local-file-access' => true,
+            'orientation' => 'Landscape',
+            'user-style-sheet' => "file:///opt/lampp/htdocs/crinaserver/WkHtmlToPdf/src/GeneratePdf/Css/style.css",
+            'outline' => true,
+            'header-html' => 'file:///opt/lampp/htdocs/crinaserver/WkHtmlToPdf/src/GeneratePdf/sections/header.html',
+            'header-spacing' => 10,
+            'footer-font-size' => 14,
+            'footer-html' => 'file:///opt/lampp/htdocs/crinaserver/WkHtmlToPdf/src/GeneratePdf/sections/header.html',
+            'footer-left' => 'the footer'
+            ];
+
+        $pdfOptions2 = [
+            'enable-local-file-access' => false,
+            'orientation' => 'Landscape',
+            'user-style-sheet' => "file:///opt/lampp/htdocs/crinaserver/WkHtmlToPdf/src/GeneratePdf/Css/style.css",
+            'outline' => true,
+            'header-spacing' => 10,
+            'footer-font-size' => 14,
+            'footer-left' => 'the footer'
+        ];
+
+        $footerHtml = 'file:///opt/lampp/htdocs/crinaserver/WkHtmlToPdf/src/GeneratePdf/sections/header.html';
+
+        $option1 = [
+            'header-right' => 'I am the Header put on the right',
+            'title' => 'Title of the generated File',
+            'header-spacing' => 1,
+            'page-size' => 'A4',
+            'orientation'      => 'Landscape',
+            'page-offset' => 2,
+            'footer-left' => 'the footer(font: 10px)',
+            'footer-line' => true,
+            'footer-font-size' => 10,
+            'user-style-sheet' => "file:///opt/lampp/htdocs/crinaserver/WkHtmlToPdf/src/GeneratePdf/Css/style.css",
+        ];
+
+        $pdf = new Pdf();
+        $pdf->addToc();
+        $pdf->setOptions($option1);
+        $pdf->addPage($content);
+        $state = $pdf->saveAs($name);
+        if(!$state){
+            die(var_dump($pdf->_error));
+        }
+        return $pdf->_isCreated;
+    }
+
+}
---
+++ tests/Unit/GeneratePdfFromHtmlPageTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\GeneratePdf\Php\HtmlToPdf;
+use PHPUnit\Framework\TestCase;
+
+class GeneratePdfFromHtmlPageTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function test_say_true()
+    {
+        $this->assertTrue(true);
+    }
+    public function test_can_generate_pdf_file()
+    {
+        //Given
+        $htmlCode = new HtmlToPdf();
+        $status = $htmlCode->buildPdf();
+        $this->assertTrue($status);
+    }
+
+
+}
